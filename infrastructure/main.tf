982a2090bdd8e63edd1e18148693bb56c20261e4
Piotr Puchala <piotrp7392@gmail.com>
2024-12-30 17:35:50 +0100

    Create basic terraform code

---
+++ infrastructure/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "mte_functions_code" {
+  bucket = "mte-functions-code"
+}
---
+++ infrastructure/versions.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      app = "mte"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "mte-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "eu-central-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.82.2"
+    }
+  }
+}
