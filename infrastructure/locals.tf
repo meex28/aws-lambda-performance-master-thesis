4b1557909b292716529d64694b9be1eaf931f195
Piotr Puchala <piotrp7392@gmail.com>
2025-02-15 12:07:25 +0100

    deploy all lambdas

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -1,33 +1,35 @@
 locals {
   lambda_functions = [
     {
-      name = "java-function-jar"
+      name               = "java-function-jar"
       bucket_source_file = "java-function-jar/function.zip"
-      handler = "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest"
-      runtime = "java21"
+      handler            = "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest"
+      runtime            = "java21"
     },
     {
-      name = "java-function-native"
+      name               = "java-function-native"
       bucket_source_file = "java-function-native/function.zip"
-      handler = "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest"
-      runtime = "provided.al2023"
+      handler            = "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest"
+      runtime            = "provided.al2023"
     },
     {
-      name = "kotlin-function-jar"
+      name               = "kotlin-function-jar"
       bucket_source_file = "kotlin-function-jar/kotlin-function-jvm-1.0-all.jar"
-      handler = "org.lambda.performance.jvm.Handler"
-      runtime = "java21"
+      handler            = "org.lambda.performance.jvm.Handler"
+      runtime            = "java21"
     },
-    # {
-    #   name = "kotlin-function-js"
-    #   bucket_source_file = "kotlin-function-js/kotlin-function-js.zip"
-    # },
     {
-      name = "kotlin-function-native"
+      name               = "kotlin-function-js"
+      bucket_source_file = "kotlin-function-js/kotlin-function-js.zip"
+      handler            = "packages/kotlin-function/kotlin/kotlin-function.handler"
+      runtime            = "nodejs22.x"
+    },
+    {
+      name               = "kotlin-function-native"
       bucket_source_file = "kotlin-function-native/kotlin-function.zip"
-      handler = "kotlin-function.kexe"
+      handler            = "kotlin-function.kexe"
       layers = [data.aws_lambda_layer_version.libcrypt_layer.arn]
-      runtime = "provided.al2023"
+      runtime            = "provided.al2023"
     },
   ]
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -21,19 +21,29 @@ resource "aws_iam_role" "this" {
 
 resource "aws_iam_policy_attachment" "this" {
   name       = "lambda-basic-policy"
-  roles      = [aws_iam_role.this.name]
+  roles = [aws_iam_role.this.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
+data "aws_s3_object" "lambda_source_code" {
+  for_each = {for lambda in local.lambda_functions : lambda.name => lambda}
+  bucket   = aws_s3_bucket.mte_functions_code.bucket
+  key      = each.value.bucket_source_file
+}
+
 resource "aws_lambda_function" "this" {
-  for_each = { for lambda in local.lambda_functions : lambda.name => lambda }
+  for_each      = {for lambda in local.lambda_functions : lambda.name => lambda}
   function_name = "mte-${each.key}"
-  s3_bucket     = aws_s3_bucket.mte_functions_code.bucket
-  s3_key        = each.value.bucket_source_file
-  handler       = each.value.handler
-  runtime       = each.value.runtime
-  layers        = try(each.value.layers, [])
   role          = aws_iam_role.this.arn
-  timeout       = 60
-  publish = true
+
+  s3_bucket        = aws_s3_bucket.mte_functions_code.bucket
+  s3_key           = each.value.bucket_source_file
+  source_code_hash = data.aws_s3_object.lambda_source_code[each.key].etag
+  publish          = true
+
+  handler = each.value.handler
+  runtime = each.value.runtime
+  layers = try(each.value.layers, [])
+
+  timeout = 60
 }
