98a4bb0e72b579be21f47e34d9713c8905d603f3
Piotr Puchala <piotrp7392@gmail.com>
2025-05-08 15:42:44 +0200

    Add Java implementation

--- java-function/src/test/java/org/lambda/performance/lambda/LambdaHandlerTest.java
+++ java-function/src/test/java/org/lambda/performance/lambda/LambdaHandlerTest.java
@@ -10,5 +10,16 @@ import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusTest
 class LambdaHandlerTest {
-
+    @Test
+    void testSimpleLambdaSuccess() throws Exception {
+        given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body("{\"request\": \"{ \\\"fistMatrix\\\": [ [1, 2, 3], [4, 5, 6] ], \\\"secondMatrix\\\": [ [7, 8], [9, 10], [11, 12] ] }\"}")
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .body(containsString("Hello, World!"));
+    }
 }
