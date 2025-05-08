a52c56cffa62c039818dd77b7141fa88da4715c5
Piotr Puchala <piotrp7392@gmail.com>
2025-05-08 16:36:14 +0200

    tmp

--- java-function/src/main/java/org/lambda/performance/RequestWrapper.java
+++ java-function/src/main/java/org/lambda/performance/RequestWrapper.java
@@ -1,7 +1,16 @@
 package org.lambda.performance;
 
+import io.quarkus.runtime.annotations.RegisterForProxy;
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+@RegisterForProxy
 public class RequestWrapper {
     private String request;
+
+    public RequestWrapper(String request) {
+        this.request = request;
+    }
 
     public String getRequest() {
         return request;
--- java-function/src/native-test/java/org/lambda/performance/lambda/LambdaHandlerTestIT.java
+++ java-function/src/native-test/java/org/lambda/performance/lambda/LambdaHandlerTestIT.java
@@ -1,9 +1,24 @@
 package org.lambda.performance.lambda;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 
 @QuarkusIntegrationTest
 public class LambdaHandlerTestIT extends LambdaHandlerTest {
 
-    // Execute the same tests but in native mode.
+    @Test
+    void testSimpleLambdaSuccess() throws Exception {
+        given()
+                .contentType("application/json")
+                .accept("application/json")
+                .body("{\"request\": \"{ \\\"fistMatrix\\\": [ [1, 2, 3], [4, 5, 6] ], \\\"secondMatrix\\\": [ [7, 8], [9, 10], [11, 12] ] }\"}")
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .body(containsString("Hello, World!"));
+    }
 }
